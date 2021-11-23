026496f307c65de08a5493e7f6944cef3e622b39
CPNV\Eliott.JAQUIER <eliott.jaquier@cpnv.ch>
2021-11-23 10:39:25 +0100

    Updated class diag + Unit tests

--- ScannergicMobile/ScannergicMobile/ScannergicMobile/ViewModels/AllergicHome.cs
+++ ScannergicMobile/ScannergicMobile/ScannergicMobile/ViewModels/AllergicHome.cs
@@ -4,28 +4,35 @@ using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 using ScannergicMobile.Models;
+using System.Threading.Tasks;
 
 namespace ScannergicMobile.ViewModels
 { 
     public class AllergicHome : BaseViewModel
     {
-        public Command AddAllergen { get; }
-
-        public ObservableCollection<Item> Items { get; }
-        public Command LoadItemsCommand { get; }
-        public Command<Item> ItemTapped { get; }
+        public Command ShowAddAllergenPageCommand { get; }
+        public ObservableCollection<Item> Allergens { get; }
+        public Command LoadAllergensCommand { get; }
+        //public Command<Item> ItemTapped { get; }
 
         public AllergicHome()
         {
             Title = "Allergies";
-            AddAllergen = new Command(ShowAddAllergen);
-
-            /*Items = new ObservableCollection<Item>();
-            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
-
+            ShowAddAllergenPageCommand = new Command(ShowAddAllergen);
+            Allergens = new ObservableCollection<Item>();
+            Item it = new Item();
+            it.Text = "Hee";
+            Allergens.Add(it);
+            Allergens.Add(it);
+            LoadAllergensCommand = new Command(async () => await ExecuteLoadAllergens());
+            /*
             ItemTapped = new Command<Item>(OnItemSelected);
 
             AddItemCommand = new Command(OnAddItem);*/
+        }
+        async Task ExecuteLoadAllergens()
+        {
+
         }
         void ShowAddAllergen()
         {
