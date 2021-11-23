0aa0425053525da7162ed8a8ac8089997655069f
CPNV\Eliott.JAQUIER <eliott.jaquier@cpnv.ch>
2021-11-23 13:30:16 +0100

    View test

--- ScannergicMobile/ScannergicMobile/ScannergicMobile/ViewModels/AllergicHome.cs
+++ ScannergicMobile/ScannergicMobile/ScannergicMobile/ViewModels/AllergicHome.cs
@@ -11,32 +11,45 @@ namespace ScannergicMobile.ViewModels
     public class AllergicHome : BaseViewModel
     {
         public Command ShowAddAllergenPageCommand { get; }
-        public ObservableCollection<Item> Allergens { get; }
+        public ObservableCollection<Item> Allergens { get; set; }
         public Command LoadAllergensCommand { get; }
-        //public Command<Item> ItemTapped { get; }
+        public Command<Item> ItemTapped { get; }
 
         public AllergicHome()
         {
             Title = "Allergies";
             ShowAddAllergenPageCommand = new Command(ShowAddAllergen);
             Allergens = new ObservableCollection<Item>();
-            Item it = new Item();
-            it.Text = "Hee";
-            Allergens.Add(it);
-            Allergens.Add(it);
+            for (int i = 0; i < 100; i++)
+            {
+                Item it = new Item();
+                it.Text = "Un item";
+                Allergens.Add(it);
+            }
+
             LoadAllergensCommand = new Command(async () => await ExecuteLoadAllergens());
-            /*
+            
             ItemTapped = new Command<Item>(OnItemSelected);
 
-            AddItemCommand = new Command(OnAddItem);*/
+            //AddItemCommand = new Command(OnAddItem);*/
         }
+
+        private void OnItemSelected(Item obj)
+        {
+            IsBusy = true;
+            int index = Allergens.IndexOf(obj);
+            obj.Text = "A";
+            Allergens[index] = obj;
+            //UpdateList();
+        }
+
         async Task ExecuteLoadAllergens()
         {
-
+            IsBusy = true;
         }
         void ShowAddAllergen()
         {
-
+            //await Shell.Current.GoToAsync("//AllergenAddView");
         }
     }
 }
