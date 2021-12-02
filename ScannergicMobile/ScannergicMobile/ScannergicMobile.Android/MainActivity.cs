d653ef669f8e11e00ad9def87e5fd20c221fa17d
CPNV\Eliott.JAQUIER <eliott.jaquier@cpnv.ch>
2021-12-02 13:27:46 +0100

    Finished polishing design

--- ScannergicMobile/ScannergicMobile/ScannergicMobile.Android/MainActivity.cs
+++ ScannergicMobile/ScannergicMobile/ScannergicMobile.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ScannergicMobile.Droid
 {
-    [Activity(Label = "ScannergicMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Scannergic", Icon = "@drawable/scannergic", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
