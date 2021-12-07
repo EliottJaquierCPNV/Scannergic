04cef36bbc52762d63b363bdd9743852c58249b0
EliottJaquier <61775618+EliottJaquierCPNV@users.noreply.github.com>
2021-12-07 09:29:50 +0100

    Product scan first test alert box

---
+++ ScannergicMobile/ScannergicMobile/ScannergicMobile/Views/ProductScan.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScannergicMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductScan : ContentPage
+    {
+        public ProductScan()
+        {
+            InitializeComponent();
+        }
+
+        void ZXingScannerView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                if(result.BarcodeFormat == ZXing.BarcodeFormat.EAN_13)
+                {
+                    DisplayAlert("Scanné!", result.Text);
+                }
+            });
+        }
+
+        private void DisplayAlert(string v1, string v2)
+        {
+            base.DisplayAlert(v1,v2,"OK");
+        }
+    }
+}
